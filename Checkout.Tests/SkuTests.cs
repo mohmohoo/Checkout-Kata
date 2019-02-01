1d48a4590f8a07acb9902acbfad37ee50901e967
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 13:07:09 +0000

    Add valid sku test

--- Checkout.Tests/SkuTests.cs
+++ Checkout.Tests/SkuTests.cs
@@ -12,9 +12,22 @@ namespace Checkout.Tests
         [TestCase('1', 100)]
         [TestCase('#', 50)]
         [TestCase('\0', 50)]
+        [TestCase('A', 0)]
+        [TestCase('A', -1)]
         public void InvalidInput_ThrowException(char name, int unitPrice)
         {
             Assert.Throws<ArgumentException>(() => new Sku(name, unitPrice));
+        }
+
+        [Test]
+        [TestCase('C', 20, 2, ExpectedResult = 40)]
+        [TestCase('D', 15, 1, ExpectedResult = 15)]
+        [TestCase('C', 10, 5, ExpectedResult = 50)]
+        [TestCase('D', 5, 5, ExpectedResult = 25)]
+        public int ValidTest(char name, int unitPrice, int itemCount)
+        {
+            var target = new Sku(name, unitPrice);
+            return target.GetPrice(new ItemCount(itemCount));
         }
     }
 }
