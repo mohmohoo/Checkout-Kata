0914683276d4524163e93bbec65d888d47f850a1
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 11:54:49 +0000

    Add constructor

--- Checkout/Store/Checkout.cs
+++ Checkout/Store/Checkout.cs
@@ -5,6 +5,13 @@ namespace Checkout.Store
     public class Checkout
         : ICheckout
     {
+        private readonly IPricing _pricing;
+
+        public Checkout(IPricing pricing)
+        {
+            _pricing = pricing;
+        }
+
         public int GetTotalPrice()
         {
             throw new NotImplementedException();
