7f34aa45dc6a386682c48d58e390f1438aaf915c
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 11:02:39 +0000

    Add implementation for discount price

--- Checkout/Store/DiscountedSku.cs
+++ Checkout/Store/DiscountedSku.cs
@@ -10,14 +10,28 @@ namespace Checkout.Store
 
         public DiscountedSku(Sku sku, int specialPriceItemCount, int specialPrice)
         {
-            _sku = sku;
-            _specialPriceItemCount = specialPriceItemCount;
-            _speicalPrice = specialPrice;
+            _sku = sku ?? throw new ArgumentException("Discounted sku cannot be null");
+
+            _specialPriceItemCount = specialPriceItemCount > 0 
+                ? specialPriceItemCount 
+                : throw new ArgumentException("Special price sku count is not valid", "specialPriceItemCount");
+
+            _speicalPrice = specialPrice > 0
+                ? specialPrice
+                : throw new ArgumentException("Special price is not valid", "specialPrice");
         }
 
         public int GetPrice(ItemCount items)
         {
-            throw new NotImplementedException();
+            var skuPrice = 0;
+            var unitPriceItemCount = items.Value % _specialPriceItemCount; 
+            if (unitPriceItemCount > 0)
+            {
+                skuPrice = _sku.GetPrice(new ItemCount(unitPriceItemCount));
+            }
+
+            var discountedItemCount = items.Value / _specialPriceItemCount;
+            return (discountedItemCount * _speicalPrice) + skuPrice;
         }
     }
 }
