97c6468b2b93b89f188f4ed9be4dfd07a874ac1d
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 13:35:03 +0000

    Add valid discounted sku tests

--- Checkout.Tests/DiscountedSkuTests.cs
+++ Checkout.Tests/DiscountedSkuTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Checkout.Store;
+using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,53 @@ using System.Threading.Tasks;
 
 namespace Checkout.Tests
 {
-    class DiscountedSkuTests
+    [TestFixture]
+    public class DiscountedSkuTests
     {
+        [Test]
+        public void NullSku_ThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => new DiscountedSku(null, 3, 100));
+        }
+
+        [Test]
+        [TestCase(0, 100)]
+        [TestCase(3, 0)]
+        [TestCase(-1, 100)]
+        [TestCase(3, -5)]
+        public void InvalidInputs_ThrowException(int specialPriceItemCount, int specialPrice)
+        {
+            Assert.Throws<ArgumentException>(() => new DiscountedSku(null, specialPriceItemCount, specialPrice));
+        }
+
+        [Test]
+        [TestCase('A', 50, 3, 130, 1, ExpectedResult = 50)]
+        [TestCase('A', 50, 3, 130, 2, ExpectedResult = 100)]
+        [TestCase('A', 50, 3, 130, 3, ExpectedResult = 130)]
+        [TestCase('A', 50, 3, 130, 4, ExpectedResult = 180)]
+        [TestCase('A', 50, 3, 130, 6, ExpectedResult = 260)]
+        [TestCase('B', 30, 2, 45, 1, ExpectedResult = 30)]
+        [TestCase('B', 30, 2, 45, 2, ExpectedResult = 45)]
+        [TestCase('B', 30, 2, 45, 3, ExpectedResult = 75)]
+        [TestCase('B', 30, 2, 45, 4, ExpectedResult = 90)]
+        [TestCase('B', 30, 2, 45, 5, ExpectedResult = 120)]
+        [TestCase('B', 30, 2, 45, 6, ExpectedResult = 135)]
+        public int ValidTests(
+            char name, 
+            int unitPrice, 
+            int specialPriceItemCount, 
+            int specialPrice, 
+            int itemCount)
+        {
+            var itemCountMock = new Mock<IItemCount>();
+            itemCountMock.Setup(x => x.Value).Returns(itemCount);
+
+            var skuMock = new Mock<IDiscountableSku>();
+            skuMock.Setup(x => x.Name).Returns(name);
+            skuMock.Setup(x => x.GetPrice(It.IsAny<IItemCount>())).Returns((itemCount % specialPriceItemCount) * unitPrice);
+            var target = new DiscountedSku(skuMock.Object, specialPriceItemCount, specialPrice);
+
+            return target.GetPrice(itemCountMock.Object);
+        }
     }
 }
