7a843498db06ffddc763e101d8f814d044b1550b
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 12:15:10 +0000

    Add checkout implementation

--- Checkout/Store/Checkout.cs
+++ Checkout/Store/Checkout.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkout.Store
 {
@@ -6,20 +8,37 @@ namespace Checkout.Store
         : ICheckout
     {
         private readonly IPricing _pricing;
+        private readonly Dictionary<string, Func<int, int>> _pricings;
+        private readonly Dictionary<string, int> _lineItems;
 
         public Checkout(IPricing pricing)
         {
             _pricing = pricing ?? throw new ArgumentException("Invalid null value", "pricing");
+
+            _pricings = new Dictionary<string, Func<int, int>>();
+            _lineItems = new Dictionary<string, int>();
         }
 
-        public int GetTotalPrice()
-        {
-            throw new NotImplementedException();
-        }
+        public int GetTotalPrice() => _lineItems.Sum(lineItem => _pricings[lineItem.Key](lineItem.Value));
 
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            var lineItems = new Dictionary<string, int>();
+            
+
+            if (lineItems.ContainsKey(item))
+            {
+                lineItems[item] += 1;
+            }
+            else
+            {
+                if (!_pricing.TryGetSku(item, out ISku sku))
+                {
+                    throw new ArgumentException("Invalid item", "item");
+                }
+
+                _pricings.Add(item, itemCount => sku.GetPrice(new ItemCount(itemCount)));
+            }
         }
     }
 }
