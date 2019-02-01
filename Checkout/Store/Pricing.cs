c1569acf12df01cd3d1595f69eed3d54264d13e6
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 11:19:19 +0000

    Add Pricing

---
+++ Checkout/Store/Pricing.cs
@@ -0,0 +1,17 @@
+﻿namespace Checkout.Store
+{
+    public class Pricing
+        : IPricing
+    {
+
+        public Pricing(params ISku[] skus)
+        {
+
+        }
+
+        public int GetPrice(int itemCount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
