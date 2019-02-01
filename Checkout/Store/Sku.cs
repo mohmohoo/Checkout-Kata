5a89ca7e9d99e7bc71748e69e41d011e430581eb
mohmohoo <mohmohoo85@gmail.com>
2019-02-01 10:49:31 +0000

    Add sku initial implementation

--- Checkout/Store/Sku.cs
+++ Checkout/Store/Sku.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 namespace Checkout.Store
 {
     public class Sku : ISku
@@ -7,13 +9,20 @@ namespace Checkout.Store
         private readonly int _unitPrice;
         public Sku(char name, int unitPrice)
         {
+            if (Regex.IsMatch(name.ToString(), "[a-z]", RegexOptions.IgnoreCase))
+            {
+                throw new ArgumentException("Sku name is not a valid alphabet character", "name");
+            }
+
+            if (unitPrice <= 0)
+            {
+                throw new ArgumentException("Unit price is not valid value", "unitPrice");
+            }
+
             _name = name;
             _unitPrice = unitPrice;
         }
 
-        public int GetPrice(ItemCount items)
-        {
-            throw new NotImplementedException();
-        }
+        public int GetPrice(ItemCount items) => items.Value * _unitPrice;
     }
 }
